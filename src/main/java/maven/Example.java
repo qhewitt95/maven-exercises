b1a5506928031e37420a069aaf551091f1cd40ad
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-08-18 10:30:43 -0500

    first commit w git ignore

---
+++ src/main/java/maven/Example.java
@@ -0,0 +1,28 @@
+package maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Example {
+
+    public static void main(String[] args) {
+        StringUtils utils = new StringUtils();
+        System.out.println(" Give us a word: ");
+        Scanner scanner = new Scanner(System.in);
+        String userWord = scanner.nextLine();
+        StringUtils.reverse(userWord);
+        System.out.printf("You Entered: %s\n", userWord);
+        String isWordANumberMessage;
+        if(StringUtils.isNumeric(userWord)){
+            isWordANumberMessage = "IS A NUMBER";
+        }else{
+            isWordANumberMessage = "IS NOT A NUMBER";
+        }
+        System.out.printf("\"%s\"%s\n,",userWord,isWordANumberMessage);
+        System.out.printf("FLipped Case: %s", StringUtils.swapCase(userWord));
+        System.out.printf("Reverse: %s", StringUtils.reverse(userWord));
+
+
+    }
+}
